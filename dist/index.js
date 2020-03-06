dc0c0c661d34dc276152704aeb26a74ef9029d70
Alejandro Gómez <amoron@emergya.com>
2020-03-06 18:09:22 +0100

    Adding the ability to connec to a WPAPersonal Wifi

--- dist/index.js
+++ dist/index.js
@@ -4,16 +4,10 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const Wifi_Factory_1 = __importDefault(require("./wifi/util/Wifi-Factory"));
-const WPAPersonalProfile_1 = __importDefault(require("./wifi/profiles/WPAPersonalProfile"));
 const WifiWrapper = {
     getWifiHandler(config) {
         return Wifi_Factory_1.default.getInstance(config);
     }
 };
 exports.default = WifiWrapper;
-const handler = WifiWrapper.getWifiHandler({ debug: true });
-const profile = new WPAPersonalProfile_1.default('Alejandro\'s iPhone', 'pixones2006');
-handler.createNetwork(profile)
-    .then(output => console.log('OK: ' + output))
-    .catch(err => console.error('ERROR:' + err));
 //# sourceMappingURL=index.js.map
